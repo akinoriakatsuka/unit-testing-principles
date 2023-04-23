a8735487447fe942bdb21dfc4b0b541ef7ef24f2
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-04-24 06:51:45 +0900

    [remove] 不要なテストメソッドを削除

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,27 +14,8 @@ use App\Models\Product;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
 
     public function test_十分な在庫があれば購入が成功する(): void
-    {
-        $store = new Store();
-        $store->addInventory(Product::Shampoo, 10);
-        $customer = new Customer();
-
-        $success = $customer->purchase($store, Product::Shampoo, 5);
-
-        assertTrue($success);
-        assertEquals(5, $store->getInventory(Product::Shampoo));
-    }
-
-    public function test_十分な在庫があれば購入が成功する_with_mock(): void
     {
         $store = $this->createMock(Store::class);
         $store->method('getInventory')->willReturn(10);
@@ -46,18 +27,6 @@ class ExampleTest extends TestCase
     }
 
     public function test_十分な在庫がなければ購入が失敗する(): void
-    {
-        $store = new Store();
-        $store->addInventory(Product::Shampoo, 10);
-        $customer = new Customer();
-
-        $success = $customer->purchase($store, Product::Shampoo, 15);
-
-        assertFalse($success);
-        assertEquals(10, $store->getInventory(Product::Shampoo));
-    }
-
-    public function test_十分な在庫がなければ購入が失敗する_with_mock(): void
     {
         $store = $this->createMock(Store::class);
         $store->method('getInventory')->willReturn(3);
