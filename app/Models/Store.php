fe6a7231987def2f0fddcca1faaf02d0c51b4598
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-04-23 23:25:17 +0900

    [fix] メソッド名をローワーキャメルケースに修正

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -11,7 +11,7 @@ class Store extends Model
 
     private $inventory = [];
 
-    public function AddInventory(Product $product,int $amount) : void {
+    public function addInventory(Product $product,int $amount) : void {
         if(isset($this->inventory[$product->name])) {
             $this->inventory[$product->name] += $amount;
         } else {
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -25,42 +25,42 @@ class ExampleTest extends TestCase
     public function test_十分な在庫があれば購入が成功する(): void
     {
         $store = new Store();
-        $store->AddInventory(Product::Shampoo, 10);
+        $store->addInventory(Product::Shampoo, 10);
         $customer = new Customer();
 
-        $success = $customer->Purchase($store, Product::Shampoo, 5);
+        $success = $customer->purchase($store, Product::Shampoo, 5);
 
         assertTrue($success);
-        assertEquals(5, $store->GetInventory(Product::Shampoo));
+        assertEquals(5, $store->getInventory(Product::Shampoo));
     }
 
     public function test_十分な在庫があれば購入が成功する_with_mock(): void
     {
         $store_mock = $this->createMock(Store::class);
-        $store_mock->method('GetInventory')->willReturn(10);
+        $store_mock->method('getInventory')->willReturn(10);
         $customer = new Customer();
-        $success = $customer->Purchase($store_mock, Product::Shampoo, 5);
+        $success = $customer->purchase($store_mock, Product::Shampoo, 5);
         assertTrue($success);
     }
 
     public function test_十分な在庫がなければ購入が失敗する(): void
     {
         $store = new Store();
-        $store->AddInventory(Product::Shampoo, 10);
+        $store->addInventory(Product::Shampoo, 10);
         $customer = new Customer();
 
-        $success = $customer->Purchase($store, Product::Shampoo, 15);
+        $success = $customer->purchase($store, Product::Shampoo, 15);
 
         assertFalse($success);
-        assertEquals(10, $store->GetInventory(Product::Shampoo));
+        assertEquals(10, $store->getInventory(Product::Shampoo));
     }
 
     public function test_十分な在庫がなければ購入が失敗する_with_mock(): void
     {
         $store_mock = $this->createMock(Store::class);
-        $store_mock->method('GetInventory')->willReturn(3);
+        $store_mock->method('getInventory')->willReturn(3);
         $customer = new Customer();
-        $success = $customer->Purchase($store_mock, Product::Shampoo, 5);
+        $success = $customer->purchase($store_mock, Product::Shampoo, 5);
         assertFalse($success);
     }
 
