8c98a779479a35b710d840028406287e3b85f4bf
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-04-26 20:39:12 +0900

    [change] テスト対象システムを$sutと命名する
    テスト対象やフェーズがわかりやすい

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -16,36 +16,35 @@ use App\Models\Caluculator;
 class ExampleTest extends TestCase
 {
 
-    public function test_十分な在庫があれば購入が成功する(): void
-    {
-        $store = $this->createMock(Store::class);
-        $store->method('getInventory')->willReturn(10);
-        $store->method('hasEnoughInventory')->willReturn(true);
-        $customer = new Customer();
-        $store->expects($this->once())->method('removeInventory');
-        $success = $customer->purchase($store, Product::Shampoo, 5);
-        assertTrue($success);
-    }
+    // public function test_十分な在庫があれば購入が成功する(): void
+    // {
+    //     $store = $this->createMock(Store::class);
+    //     $store->method('getInventory')->willReturn(10);
+    //     $store->method('hasEnoughInventory')->willReturn(true);
+    //     $customer = new Customer();
+    //     $store->expects($this->once())->method('removeInventory');
+    //     $success = $customer->purchase($store, Product::Shampoo, 5);
+    //     assertTrue($success);
+    // }
 
-    public function test_十分な在庫がなければ購入が失敗する(): void
-    {
-        $store = $this->createMock(Store::class);
-        $store->method('getInventory')->willReturn(3);
-        $store->method('hasEnoughInventory')->willReturn(false);
-        $customer = new Customer();
-        $store->expects($this->never())->method('removeInventory');
-        $success = $customer->purchase($store, Product::Shampoo, 5);
-        assertFalse($success);
-    }
+    // public function test_十分な在庫がなければ購入が失敗する(): void
+    // {
+    //     $store = $this->createMock(Store::class);
+    //     $store->method('getInventory')->willReturn(3);
+    //     $store->method('hasEnoughInventory')->willReturn(false);
+    //     $customer = new Customer();
+    //     $store->expects($this->never())->method('removeInventory');
+    //     $success = $customer->purchase($store, Product::Shampoo, 5);
+    //     assertFalse($success);
+    // }
 
     public function test_足し算(): void
     {
         $first = 10.0;
         $second = 20.0;
-        $caluculator = new Caluculator();
+        $sut = new Caluculator();
 
-        $result = $caluculator->sum($first, $second);
+        $result = $sut->sum($first, $second);
         assertEquals(30.0,$result);
-
     }
 }
