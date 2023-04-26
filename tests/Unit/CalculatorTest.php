08bc211e6f2e24fa2ac8708982c605e1ebd560ed
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-04-26 20:56:47 +0900

    [add] セットアップメソッドを使用

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -8,13 +8,19 @@ use App\Models\Caluculator;
 
 class CalculatorTest extends TestCase
 {
+    private $_sut;
+
+    public function setUp() :void  {
+        $this->_sut = new Caluculator();
+    }
+
     public function test_足し算(): void
     {
         $first = 10.0;
         $second = 20.0;
-        $sut = new Caluculator();
+        // $sut = new Caluculator();
 
-        $result = $sut->sum($first, $second);
+        $result = $this->_sut->sum($first, $second);
         assertEquals(30.0,$result);
     }
 }
